d8a8b561b20a85029296eff0d24c308bc8822e3e
Renan Menezes <renancarmomenezes@gmail.com>
2023-12-06 13:50:26 +0400

    Feat: Search

---
+++ script.js
@@ -0,0 +1,31 @@
+const sun = document.querySelector('.icon_sun');
+const hearts = document.querySelectorAll('.icon_heart');
+const articleContainers = document.querySelectorAll('.article_container');
+
+sun.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  // Trocar a imagem do sol para a lua e vice-versa
+  sun.src = document.body.classList.contains('dark') ? '/assets/Images/Moon.svg' : '/assets/Images/SunDim.svg';
+});
+
+hearts.forEach((heart) => {
+  heart.addEventListener('click', () => {
+    heart.classList.toggle('filled');
+    // Trocar a imagem do coração para preenchido e vice-versa
+    heart.src = heart.classList.contains('filled') ? '/assets/Images/HeartFilled.svg' : '/assets/Images/Heart.svg';
+  });
+});
+
+searchInput.addEventListener('input', () => {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  articleContainers.forEach((articleContainer) => {
+    const articleText = articleContainer.querySelector('.article_text').textContent.toLowerCase();
+
+    if (articleText.includes(searchTerm)) {
+      articleContainer.style.display = 'flex';
+    } else {
+      articleContainer.style.display = 'none';
+    }
+  });
+});
